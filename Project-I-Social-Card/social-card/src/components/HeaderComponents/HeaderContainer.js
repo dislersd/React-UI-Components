077acf6398dc16b5239b7a116941ce48ed908b8b
dislersd <dislersd@gmail.com>
2019-02-11 12:38:35 -0800

    created HeaderContainer, HeaderTitle, ImageThumbnail, HeaderContent components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+  return (
+    <div>
+      <ImageThumbnail />
+    </div>
+  );
+}
+
+export default HeaderContainer;
