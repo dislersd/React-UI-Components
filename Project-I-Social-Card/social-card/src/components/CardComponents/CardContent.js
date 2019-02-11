f7a4570956dda380d3591fe6aebfa4a2fefeae26
dislersd <dislersd@gmail.com>
2019-02-11 13:25:49 -0800

    built all card components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+  return (
+    <div>
+      <h2>Get Started With React</h2>
+      <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+      <a href="#">react.js</a>
+    </div>
+  );
+}
+
+export default CardContent;
